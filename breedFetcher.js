70453724f5ad25f6d2e2e570cdc28f3793f0b8a5
Ryan Kang <rkang626@gmail.com>
2021-01-18 17:53:21 +0000

    add unit tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,17 @@
 const request = require('request');
 
-const inputBreed = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error !== null) {
+      return callback(error, null);
+    } else if (response.statusCode !== 200 && response.statusCode !== undefined) {
+      return callback(`Status Code: ${response.statusCode}`, null);
+    } else if (body === '[]') {
+      return callback('Breed not found.', null);
+    } else {
+      return callback(null, JSON.parse(body)[0]['description']);
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${inputBreed}`, (error, response, body) => {
-  if (error !== null) {
-    console.log(error);
-  } else if (response.statusCode !== 200 && response.statusCode !== undefined) {
-    console.log(`Status Code: ${response.statusCode}`);
-  } else if (body === '[]') {
-    console.log('Breed not found.');
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0]['description']);
-  }
-});
+module.exports = { fetchBreedDescription };
